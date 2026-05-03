f3af908e474b358ecb68b8884c09171ae5ae5baa
Richard Kichenama <rkichenama@gmail.com>
2026-05-03 19:51:47 -0400

    feat: add StrandCount and CurrentInterweaved signals

--- src/data/CurrentKnot.ts
+++ src/data/CurrentKnot.ts
@@ -1,8 +1,11 @@
 import { signal } from '@preact/signals-react';
-import { Knot } from '../lib';
+import { InterweavedKnot, Knot } from '../lib';
 
 export const CurrentKnot = (window['knot'] = signal<Knot>(new Knot({
   bights: 6, parts: 5
 })));
 
 export const KnotError = signal<string>('');
+
+export const StrandCount = signal<number>(1);
+export const CurrentInterweaved = signal<InterweavedKnot | null>(null);
